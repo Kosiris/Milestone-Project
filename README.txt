1d1c74299a41b88ec39888941ee20f484321cc5c
Cam <farrcameron@gmail.com>
2023-09-23 09:27:07 -0700

    Update

---
+++ README.txt
@@ -0,0 +1,1 @@
+** Code contributers * Noah
--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const playBoard = document.querySelector(".board");
 
 let foodX, foodY;
-let snakeX = 5, snakeY = 10;
-let snakeBody = []
+let snakeX = 10, snakeY = 14;
+let snakeBody = [];
 let velocityX = 0, velocityY = 0; 
 
+//Randomizing the food position using Math.floor
 const changeFoodPosition = () => {
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 }
-
+//Changing position based on velocity from key presses.
 const changeDirection = (e) => {
     if(e.key === "ArrowUp"){
         velocityX = 0;
@@ -26,18 +27,18 @@ const changeDirection = (e) => {
     }
     initGame();
 }
-
+// food positioning and allowing the food position to be pushed to snake body
 const initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
     if(snakeX === foodX && snakeY === foodY){
         changeFoodPosition();
-
+        snakeBody.push([foodX, foodY]); 
     }
 
     snakeX += velocityX;
     snakeY += velocityY;
-    htmlMarkup += `<div class="food" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
+    htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
     playBoard.innerHTML = htmlMarkup;
 }
 changeFoodPosition()
